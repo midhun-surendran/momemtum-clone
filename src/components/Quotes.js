64df5517110e4239ac5439411535e4c4561001c1
Midhun Surendran <midhunsurendran4558@gmail.com>
2022-06-16 16:48:42 +0530

    padding added to quotes

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -17,7 +17,9 @@ function Quotes() {
   if (!data) return null;
   return (
     <div className="max-w-xl lg:max-w-5xl w-full h-auto  bg-black  flex flex-col justify-center items-center mt-16  shadow-xl bg-clip-padding backdrop-filter bg-opacity-10 backdrop-blur-md rounded-md ">
-      <p className="leading 7 text-lg md:text-2xl">"{data.quotes[0].text}"</p>
+      <p className="leading 7 text-lg md:text-2xl p-10">
+        "{data.quotes[0].text}"
+      </p>
       <p className="text-lg">-{data.quotes[0].author}</p>
     </div>
   );
